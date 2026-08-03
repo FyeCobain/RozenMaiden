813546a1fc9004121c8cadd5b395f81f38aa251a
Rozen <hereisnirvana@hotmail.com>
2026-08-02 23:21:53 -0600

    chore(icons): add icon converter script

---
+++ src/app/icons/index.ts
@@ -0,0 +1,22 @@
+import ROSE_ICON_CODE from './rose.icon';
+import ROSES_ICON_CODE from './roses.icon';
+import ROZA_MYSTICA_OUTLINE_ICON_CODE from './roza-mystica-outline.icon';
+import ROZA_MYSTICA_ICON_CODE from './roza-mystica.icon';
+import SUIGINTOU_FEATHER_1_ICON_CODE from './suigintou-feather-1.icon';
+import SUIGINTOU_FEATHER_2_ICON_CODE from './suigintou-feather-2.icon';
+import SUIGINTOU_FEATHER_3_ICON_CODE from './suigintou-feather-3.icon';
+import SUIGINTOU_FEATHER_4_ICON_CODE from './suigintou-feather-4.icon';
+
+export type IconName = 'rose' | 'roses' | 'roza_mystica_outline' | 'roza_mystica' | 'suigintou_feather_1' | 'suigintou_feather_2'
+ | 'suigintou_feather_3' | 'suigintou_feather_4';
+
+export const Icons: Record<IconName, string> = {
+  'rose': ROSE_ICON_CODE,
+  'roses': ROSES_ICON_CODE,
+  'roza_mystica_outline': ROZA_MYSTICA_OUTLINE_ICON_CODE,
+  'roza_mystica': ROZA_MYSTICA_ICON_CODE,
+  'suigintou_feather_1': SUIGINTOU_FEATHER_1_ICON_CODE,
+  'suigintou_feather_2': SUIGINTOU_FEATHER_2_ICON_CODE,
+  'suigintou_feather_3': SUIGINTOU_FEATHER_3_ICON_CODE,
+  'suigintou_feather_4': SUIGINTOU_FEATHER_4_ICON_CODE,
+};
