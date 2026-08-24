da14f121b0310d85e0b2fe42017d889329756782
Rozen <hereisnirvana@hotmail.com>
2026-08-23 22:36:48 -0600

    chore(icons): improve icons converter script

--- src/app/icons/index.ts
+++ src/app/icons/index.ts
@@ -1,22 +1,24 @@
-import ROSE_ICON_CODE from './rose.icon';
-import ROSES_ICON_CODE from './roses.icon';
-import ROZA_MYSTICA_OUTLINE_ICON_CODE from './roza-mystica-outline.icon';
-import ROZA_MYSTICA_ICON_CODE from './roza-mystica.icon';
-import SUIGINTOU_FEATHER_1_ICON_CODE from './suigintou-feather-1.icon';
-import SUIGINTOU_FEATHER_2_ICON_CODE from './suigintou-feather-2.icon';
-import SUIGINTOU_FEATHER_3_ICON_CODE from './suigintou-feather-3.icon';
-import SUIGINTOU_FEATHER_4_ICON_CODE from './suigintou-feather-4.icon';
+import ROSE_ICON from './rose.icon';
+import ROSES_ICON from './roses.icon';
+import ROZA_MYSTICA_OUTLINE_ICON from './roza-mystica-outline.icon';
+import ROZA_MYSTICA_ICON from './roza-mystica.icon';
+import SUIGINTOU_FEATHER_1_ICON from './suigintou-feather-1.icon';
+import SUIGINTOU_FEATHER_2_ICON from './suigintou-feather-2.icon';
+import SUIGINTOU_FEATHER_3_ICON from './suigintou-feather-3.icon';
+import SUIGINTOU_FEATHER_4_ICON from './suigintou-feather-4.icon';
+
+export type Icon = { width: number, height: number, code: string };
 
 export type IconName = 'rose' | 'roses' | 'roza_mystica_outline' | 'roza_mystica' | 'suigintou_feather_1' | 'suigintou_feather_2'
  | 'suigintou_feather_3' | 'suigintou_feather_4';
 
-export const Icons: Record<IconName, string> = {
-  'rose': ROSE_ICON_CODE,
-  'roses': ROSES_ICON_CODE,
-  'roza_mystica_outline': ROZA_MYSTICA_OUTLINE_ICON_CODE,
-  'roza_mystica': ROZA_MYSTICA_ICON_CODE,
-  'suigintou_feather_1': SUIGINTOU_FEATHER_1_ICON_CODE,
-  'suigintou_feather_2': SUIGINTOU_FEATHER_2_ICON_CODE,
-  'suigintou_feather_3': SUIGINTOU_FEATHER_3_ICON_CODE,
-  'suigintou_feather_4': SUIGINTOU_FEATHER_4_ICON_CODE,
+export const icons: Record<IconName, Icon> = {
+  'rose': ROSE_ICON,
+  'roses': ROSES_ICON,
+  'roza_mystica_outline': ROZA_MYSTICA_OUTLINE_ICON,
+  'roza_mystica': ROZA_MYSTICA_ICON,
+  'suigintou_feather_1': SUIGINTOU_FEATHER_1_ICON,
+  'suigintou_feather_2': SUIGINTOU_FEATHER_2_ICON,
+  'suigintou_feather_3': SUIGINTOU_FEATHER_3_ICON,
+  'suigintou_feather_4': SUIGINTOU_FEATHER_4_ICON,
 };
